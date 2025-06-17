c00a7ce5489cf4404bec70bc1edec7f096c0b0f9
chaves <chavesprogrammer@gmail.com>
2025-06-17 16:59:41 -0300

    fazendo requisicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,63 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Search from './components/Search'
+
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+// const API_KEY = import.meta.env.VITE_API_KEY;
+
+// const API_OPTIONS = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: `Bearer ${API_KEY}`
+//   }
+// }
+
+const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MzlkZDliNzUxN2VkY2RhNTkyNTRhODdlZjljYzBlOSIsIm5iZiI6MTc1MDE4MzcxMy42MTMsInN1YiI6IjY4NTFhZjIxZmIyNWY4MGUwY2JiZDkxMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hestRH_pe7C08Eu4fxDp0ome3jChnRPcDJMkgzBmHhQ'
+  }
+};
 
 function App() {
 
   const [searchTerm, setSearchTerm] = useState<string>('');
 
+  const fetchMovies = async () => {
+
+    try {
+      const endpoint = url;
+
+      const res = await fetch(endpoint, options);
+
+      if (!res.ok) {
+        throw new Error('Erro ao buscar filmes');
+      }
+
+      const data = await res.json();
+      
+      console.log('Filmes encontrados:', data);
+
+      return data;
+
+    }catch (error){
+      console.error('Erro ao buscar filmes:', error);
+      return null;
+    }
+  }
+
+  useEffect( () => {
+      fetchMovies();
+  }, []);
+
   return(
     <main>
-      
+
       <div className='pattern'/>
 
       <div className='wrapper'>
@@ -18,9 +67,14 @@ function App() {
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Procure <span className='text-gradient'>Filmes</span> que você ama sem problemas!</h1>
 
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />  
+
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />  
+        <section className='all-movies'>
+          <h2>Todos Filmes</h2>
+
+        </section>
 
         <h1 className='text-3xl'> {searchTerm} </h1>
 
