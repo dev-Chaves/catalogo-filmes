79956778f2ba785bb3d9fe49462194326d321090
chaves <chavesprogrammer@gmail.com>
2025-06-17 12:04:59 -0300

    inicio

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return(
+    <main>
+      <div className='pattern'/>
+
+      <div className='wrapper'>
+        <header>
+          <h1>Procure <span className='text-gradient'>Filmes</span> que você ama sem problemas!</h1>
+        </header>
+      </div>
+    </main>
+  )
+}
+
+export default App
+  
